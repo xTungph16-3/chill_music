1ba878fdfc65f6fa81674b9b2fb90b997b485409
Admin <tungne2k4@gmail.com>
2024-08-31 21:31:15 +0700

    Initial commit

--- js/music-list.js
+++ js/music-list.js
@@ -1,205 +1,235 @@
 let allMusic = [{
-    name:  "Trời Giấu Trời Mang Đi",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-1",
-    src: "music-1"
-},
-{
-    name:  "Giấc Mơ Rất Thơ",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-2",
-    src: "music-2"
-},
-{
-    name:  "Tình Thật Tình Mơ",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-3",
-    src: "music-3"
-},
-{
-    name:  "Đắp Mộ Magaming",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-4",
-    src: "music-4"
-},
-{
-    name:  "Thương Cho Tấm Thân",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-5",
-    src: "music-5"
-},
-{
-    name: "Thà Yêu Lấy Một Người Bình Thường",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-6",
-    src: "music-6"
-},
-{
-    name:  "Ngàn Câu Hứa",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-7",
-    src: "music-7"
-},
-{
-    name:  "Nắm Tay Nhau",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-8",
-    src: "music-8"
-},
-{
-    name:  "Sợ Ngày Mai Em Đi Mất",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-9",
-    src: "music-9"
-},
-{
-    name:  "Trót Trao Duyên",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-10",
-    src: "music-10"
-},
-{
-    name:  "Ngày Thơ ẤU",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-11",
-    src: "music-11"
-},
-{
-    name:  "Cô Gái Này Là Của Ai",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-12",
-    src: "music-12"
-},
-{
-    name:  "Gió",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-13",
-    src: "music-13"
-},
-{
-    name:  "i love you",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-14",
-    src: "music-14"
-},
-{
-    name:  "Bông Hoa Đẹp Nhất",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-15",
-    src: "music-15"
-},
-{
-    name:  "Bông Hoa Chẳng Thuộc Về Ta",
-    artist:"Trịnh Xuân Tùng      ",
-    img: "music-16",
-    src: "music-16"
-},
-{
-    name:  "Ghosting",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-17",
-    src:"music-17"
-},
-{
-    name:  "Lan man",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-18",
-    src:"music-18"
-},
-{
-    name:  "100 Years LOVE",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-19",
-    src:"music-19"
-},
-{
-    name:  "Nhất trên đời",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-20",
-    src:"music-20"
-},
-{
-    name:  "Có chắc yêu là đây",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-21",
-    src:"music-21"
-},
-{
-    name:  "Lạc Vào Trong Mơ",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-22",
-    src:"music-22"
-},
-{
-    name:  "Ai Đưa Em Về",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-23",
-    src:"music-23"
-},
-{
-    name:  "Kìa Bóng Dáng Ai",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-24",
-    src:"music-24"
-},
-{
-    name:  "2 AM",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-25",
-    src:"music-25"
-},
-{
-    name:  "Chúng ta của hiện tại",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-26",
-    src:"music-26"
-},
-{
-    name:  "Yêu anh thế thôi",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-27",
-    src:"music-27"
-},
-{
-    name:  "Hẹn em ở lần yêu thứ 2",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-28",
-    src:"music-28"
-},
-{
-    name:  "Chẳng còn thời gian ấy",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-29",
-    src:"music-29"
-},
-{
-    name:  "Có thể hay không",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-30",
-    src:"music-30"
-},
-{
-    name:  "Làm thân đê",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-31",
-    src:"music-31"
-},
-{
-    name:  "Có hẹn với thanh xuân",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-32",
-    src:"music-32"
-},
-{
-    name:  "Bước nhẹ vào tim anh",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-33",
-    src:"music-33"
-},
-{
-    name:  "Ngày đẹp trời nói lời chia tay",
-    artist:"Trịnh Xuân Tùng      ",
-    img:"music-34",
-    src:"music-34"
-}
+        name:  "Ngày đẹp trời nói lời chia tay",
+        artist:"Trịnh Xuân Tùng      ",
+        img:"music-1",
+        src:"music-1"
+    },
+    {
+        name:  "Bước nhẹ vào tim anh",
+        artist:"Trịnh Xuân Tùng      ",
+        img:"music-2",
+        src:"music-2"
+    },
+    {
+        name:  "Cảm ơn vì đã yêu",
+        artist:"Trịnh Xuân Tùng      ",
+        img:"music-3",
+        src:"music-3"
+    },
+    {
+        name:  "Cạn tình như thế",
+        artist:"Trịnh Xuân Tùng      ",
+        img:"music-4",
+        src:"music-4"
+    },
+    {
+        name:  "Từng là",
+        artist:"Trịnh Xuân Tùng      ",
+        img:"music-5",
+        src:"music-5"
+    }
+
+//     name:  "Trời Giấu Trời Mang Đi",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-1",
+//     src: "music-1"
+// },
+// {
+//     name:  "Giấc Mơ Rất Thơ",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-2",
+//     src: "music-2"
+// },
+// {
+//     name:  "Tình Thật Tình Mơ",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-3",
+//     src: "music-3"
+// },
+// {
+//     name:  "Đắp Mộ Magaming",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-4",
+//     src: "music-4"
+// },
+// {
+//     name:  "Thương Cho Tấm Thân",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-5",
+//     src: "music-5"
+// },
+// {
+//     name: "Thà Yêu Lấy Một Người Bình Thường",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-6",
+//     src: "music-6"
+// },
+// {
+//     name:  "Ngàn Câu Hứa",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-7",
+//     src: "music-7"
+// },
+// {
+//     name:  "Nắm Tay Nhau",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-8",
+//     src: "music-8"
+// },
+// {
+//     name:  "Sợ Ngày Mai Em Đi Mất",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-9",
+//     src: "music-9"
+// },
+// {
+//     name:  "Trót Trao Duyên",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-10",
+//     src: "music-10"
+// },
+// {
+//     name:  "Ngày Thơ ẤU",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-11",
+//     src: "music-11"
+// },
+// {
+//     name:  "Cô Gái Này Là Của Ai",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-12",
+//     src: "music-12"
+// },
+// {
+//     name:  "Gió",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-13",
+//     src: "music-13"
+// },
+// {
+//     name:  "i love you",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-14",
+//     src: "music-14"
+// },
+// {
+//     name:  "Bông Hoa Đẹp Nhất",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-15",
+//     src: "music-15"
+// },
+// {
+//     name:  "Bông Hoa Chẳng Thuộc Về Ta",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img: "music-16",
+//     src: "music-16"
+// },
+// {
+//     name:  "Ghosting",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-17",
+//     src:"music-17"
+// },
+// {
+//     name:  "Lan man",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-18",
+//     src:"music-18"
+// },
+// {
+//     name:  "100 Years LOVE",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-19",
+//     src:"music-19"
+// },
+// {
+//     name:  "Nhất trên đời",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-20",
+//     src:"music-20"
+// },
+// {
+//     name:  "Có chắc yêu là đây",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-21",
+//     src:"music-21"
+// },
+// {
+//     name:  "Lạc Vào Trong Mơ",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-22",
+//     src:"music-22"
+// },
+// {
+//     name:  "Ai Đưa Em Về",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-23",
+//     src:"music-23"
+// },
+// {
+//     name:  "Kìa Bóng Dáng Ai",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-24",
+//     src:"music-24"
+// },
+// {
+//     name:  "2 AM",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-25",
+//     src:"music-25"
+// },
+// {
+//     name:  "Chúng ta của hiện tại",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-26",
+//     src:"music-26"
+// },
+// {
+//     name:  "Yêu anh thế thôi",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-27",
+//     src:"music-27"
+// },
+// {
+//     name:  "Hẹn em ở lần yêu thứ 2",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-28",
+//     src:"music-28"
+// },
+// {
+//     name:  "Chẳng còn thời gian ấy",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-29",
+//     src:"music-29"
+// },
+// {
+//     name:  "Có thể hay không",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-30",
+//     src:"music-30"
+// },
+// {
+//     name:  "Làm thân đê",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-31",
+//     src:"music-31"
+// },
+// {
+//     name:  "Có hẹn với thanh xuân",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-32",
+//     src:"music-32"
+// },
+// {
+//     name:  "Bước nhẹ vào tim anh",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-33",
+//     src:"music-33"
+// },
+// {
+//     name:  "Ngày đẹp trời nói lời chia tay",
+//     artist:"Trịnh Xuân Tùng      ",
+//     img:"music-34",
+//     src:"music-34"
+// }
 ];
